a27e4c2188742ad1cf92b46ddb8c5b4b458204f8
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2020-09-19 13:48:57 +0300

    Initial

---
+++ HelloWorld/HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace HelloWorld
+{ 
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+
+            this.slider.Value = 0.5;
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            label.Text = string.Format("Value is {0:f2}", e.NewValue);
+        }
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            this.DisplayAlert("Hello title", "Hello! How are you!", "Fine, thank you!", "Not fine...");
+        }
+    }
+}
