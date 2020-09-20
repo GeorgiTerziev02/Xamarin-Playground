ada920dd2df95c883b9dedc0cfdd1a9929939540
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2020-09-20 14:26:10 +0300

    xaml lesson

--- HelloWorld/HelloWorld/HelloWorld/GreetPage.xaml.cs
+++ HelloWorld/HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -1,11 +1,23 @@
 ﻿namespace HelloWorld
-{ 
+{
+    using System.Collections.Generic;
     using Xamarin.Forms;
     using Xamarin.Forms.Xaml;
 
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class GreetPage : ContentPage
     {
+        protected List<string> texts = new List<string>()
+        {
+            "Hello",
+            "How are you",
+            "I am good",
+            "aaaa",
+            "Start over again"
+        };
+
+        protected int index = -1;
+
         public GreetPage()
         {
             InitializeComponent();
@@ -13,14 +25,25 @@
             this.slider.Value = 0.5;
         }
 
-        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            label.Text = string.Format("Value is {0:f2}", e.NewValue);
-        }
-
         private void Button_Clicked(object sender, System.EventArgs e)
         {
             this.DisplayAlert("Hello title", "Hello! How are you!", "Fine, thank you!", "Not fine...");
+        }
+
+        private void NextText(object sender, System.EventArgs e)
+        {
+            index++;
+            if(index >= this.texts.Count)
+            {
+                index = 0;
+            }
+
+            this.label.Text = this.texts[index];
+        }
+
+        private void ChangeFont(object sender, ValueChangedEventArgs e)
+        {
+            this.label.FontSize = e.NewValue * 25;
         }
     }
 }
