b9d24a09ed295f0bb6c3bd84779907e5c9a2d63e
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2020-09-22 23:18:17 +0300

    context actions

--- HelloWorld/HelloWorld/HelloWorld/ListPage.xaml.cs
+++ HelloWorld/HelloWorld/HelloWorld/ListPage.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using HelloWorld.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -13,23 +11,35 @@ namespace HelloWorld
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListPage : ContentPage
     {
+        private ObservableCollection<Contact> _contacts;
+
         public ListPage()
         {
             InitializeComponent();
 
-            this.listView.ItemsSource = new List<ContactGroup> 
+            _contacts = new ObservableCollection<Contact> 
             {
-                new ContactGroup("A", "A")
-                {
                    new Contact{Name="AAA", ImageUrl="https://onlinejpgtools.com/images/examples-onlinejpgtools/coffee-resized.jpg", Status="AAA"},
-                   new Contact{Name="ABC", ImageUrl="https://onlinejpgtools.com/images/examples-onlinejpgtools/coffee-resized.jpg"}
-                },
-                new ContactGroup("B", "B")
-                {
+                   new Contact{Name="ABC", ImageUrl="https://onlinejpgtools.com/images/examples-onlinejpgtools/coffee-resized.jpg"},   
                    new Contact{Name="BBB", ImageUrl="https://onlinejpgtools.com/images/examples-onlinejpgtools/coffee-resized.jpg"}
-                }
                 //new Contact{Name="AAA", ImageUrl="https://onlinejpgtools.com/images/examples-onlinejpgtools/coffee-resized.jpg"},
             };
+
+            listView.ItemsSource = _contacts;
+        }
+
+        private void CallClicked(object sender, EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            var contact = menuItem.CommandParameter as Contact;
+
+            DisplayAlert("Call", contact.Name, "OK");
+        }
+
+        private void DeleteClicked(object sender, EventArgs e)
+        {
+            var contact = (sender as MenuItem).CommandParameter as Contact;
+            _contacts.Remove(contact);
         }
     }
 }
